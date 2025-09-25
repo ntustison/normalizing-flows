657876c2947f2a4b707ad00bc1664dc6ccf417c8
Nick Tustison <ntustison@gmail.com>
2025-09-25 11:37:28 -0700

    BUG:  Fix build process.

--- src/normflows/__init__.py
+++ src/normflows/__init__.py
@@ -1,2 +1,16 @@
-__all__ = []
-__version__ = "0.1.0"
+from ._version import __version__  # light import
+
+# Avoid importing torch-consuming modules at build time
+try:
+    from .core import NormalizingFlow, MultiscaleFlow
+except Exception:
+    NormalizingFlow = None
+    MultiscaleFlow = None
+
+from . import flows, distributions, nets, sampling, utils  # optional; guard similarly if needed
+
+__all__ = [
+    "NormalizingFlow", "MultiscaleFlow",
+    "flows", "distributions", "nets", "sampling", "utils",
+    "__version__",
+]
